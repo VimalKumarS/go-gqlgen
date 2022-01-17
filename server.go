f3d51e50e7035e91a36b3c2346af09cefbbf737e
vimalkumarsinghal@gmail.com <vimalkumarsinghal@gmail.com>
2022-01-16 17:39:20 -0800

    removed comment code

--- server.go
+++ server.go
@@ -32,19 +32,5 @@ func main() {
 	log.Fatal(http.ListenAndServe(":"+port, router))
 
 
-	//port := os.Getenv("PORT")
-	//if port == "" {
-	//	port = defaultPort
-	//}
-	//
-	//router := chi.NewRouter()
-
-
-	//database.Migrate()
-	//server := handler.NewDefaultServer(hackernews.NewExecutableSchema(hackernews.Config{Resolvers: &hackernews.Resolver{}}))
-	//router.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	//router.Handle("/query", server)
-	//
-	//log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	//log.Fatal(http.ListenAndServe(":"+port, router))
+	
 }
